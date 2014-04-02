df002eac7ff68b54f9eecdf84f76347291088d1a
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2014-04-02 16:07:51 +0530

    Fix the arguments for assertEqual
    Order of arguments should be: actual, expected
    
    Change-Id: Icc866ac7c05cf19f996ac94b27057788eb03a3ac

--- tests/segmentation/CXSegmenter.test.js
+++ tests/segmentation/CXSegmenter.test.js
@@ -6,18 +6,18 @@ QUnit.test( 'Segmentation tests', function ( assert ) {
 		allTests = require( './SegmentationTests.json' );
 
 	for ( lang in allTests ) {
-		count += allTests[lang].length;
+		count += allTests[ lang ].length;
 	}
 	QUnit.expect( count );
 	for ( lang in allTests ) {
-		tests = allTests[lang];
-		for ( i = 0, len = allTests[lang].length; i < len; i++ ) {
-			test = tests[i];
+		tests = allTests[ lang ];
+		for ( i = 0, len = allTests[ lang ].length; i < len; i++ ) {
+			test = tests[ i ];
 			segmenter = new CX.Segmenter( test.source, lang );
 			segmenter.segment();
 			result = segmenter.getSegmentedContent();
 			result = result.replace( /(\r\n|\n|\t|\r)/gm, '' );
-			assert.strictEqual( test.result, result );
+			assert.strictEqual( result, test.result );
 		}
 	}
 } );
