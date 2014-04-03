5cb1c9ae6179d916cbcd73e7fc3b95a687407c6a
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2014-04-03 17:06:37 +0530

    Segmentation: Handle the case of repeated references
    Example: This is a sentence.[1][2][3][4]
    Added more tests to test different possible scenarios
    
    Change-Id: I992d9e02157e0649ff8bcb02992206c8a793548d

--- segmentation/languages/CXParser.js
+++ segmentation/languages/CXParser.js
@@ -78,6 +78,7 @@ function entity( str ) {
  */
 CXParser.prototype.startSentence = function () {
 	this.inSentence = true;
+	this.sawSentenceEndCandidate = false;
 	return '\n\t<span class="cx-segment" data-segmentid="' + ( this.segmentCount++ ) + '">';
 };
 
@@ -90,11 +91,20 @@ CXParser.prototype.endSentence = function () {
 };
 
 /**
+ * Undo end a sentence
+ */
+CXParser.prototype.undoEndSentence = function () {
+	this.inSentence = true;
+	this.segmentedContent = this.segmentedContent.substr( 0, this.segmentedContent.length - 7 );
+};
+
+/**
  * Text handler
  * @param {string} text
  */
 CXParser.prototype.ontext = function ( text ) {
 	var parser = this;
+
 	if ( !text.trim() ) {
 		return;
 	}
@@ -107,7 +117,6 @@ CXParser.prototype.ontext = function ( text ) {
 		var replacement, nextLetter;
 
 		replacement = prevWord + sentenceSeparator;
-		//console.log([match, prevWord, sentenceSeparator, offset]);
 		nextLetter = sentence[ offset + match.length ];
 		if ( prevWord && prevWord.length < 3 && prevWord[ 0 ].toUpperCase() === prevWord[ 0 ] ||
 			nextLetter && nextLetter.toLowerCase() === nextLetter ) {
@@ -120,10 +129,12 @@ CXParser.prototype.ontext = function ( text ) {
 	}
 
 	text = text.replace( /(\w*)([.!?][\s])/g, textSplit );
-	// content terminating with [.|!|?]
+	// content terminating with [.|!|?]. But defer the decision of sentence break
+	// to handle cases like: "Hydrogen is a gas.[1] It is an..". References part of
+	// the sentence appear after the period.
 	text = text.replace( /([.!?])$/, function ( match, p1 ) {
-		parser.seenSentenceEnd = true;
-		return p1; // + parser.endSentence();
+		parser.sawSentenceEndCandidate = true;
+		return p1;
 	} );
 	this.print( text );
 };
@@ -155,17 +166,36 @@ CXParser.prototype.onopentag = function ( tag ) {
 			this.print( this.endSentence() );
 		}
 	}
+
 	if ( tag.name === 'a' && !this.inSentence ) {
 		// sentences starting with a link
 		this.print( this.startSentence() );
 	}
 
-	if ( tag.name === 'span' && this.seenSentenceEnd &&
-		tag.attributes.class === 'reference' && this.inSentence ) {
-		// Sentences staring with reference links.
-		// Example: Sentence one.[1] Sentence two
-		// Here [1] is not part of Sentence two. It is reference for Sentence one.
-		this.inReference = true;
+	if ( this.sawSentenceEndCandidate ) {
+		if ( tag.name === 'span' &&
+			tag.attributes.class === 'reference' && ( this.inSentence || this.inReference ) && this.sawSentenceEndCandidate ) {
+			// Sentences staring with reference links.
+			// Example: Sentence one.[1] Sentence two
+			// Here [1] is not part of Sentence two. It is reference for Sentence one.
+			// It is also possible to have this references repeated n times
+			// Example: Sentence one.[1][2][3][4] Sentence two
+			if ( this.inReference ) {
+				// This is already in Reference state. That means, this is a
+				// case of multiple references. We need to remove the last sentence close
+				this.inSentence = true;
+				this.undoEndSentence();
+			}
+			this.inReference = true;
+		}
+
+		// Check if we need to reset inReference state. References contains an 'a' tag
+		// inside 'span' tag
+		if ( this.inReference && !( tag.name === 'a' || tag.name === 'span' ) ) {
+			// Reset inReference
+			this.inReference = false;
+			this.sawSentenceEndCandidate = false;
+		}
 	}
 	// Start of tag
 	this.print( '<' + tag.name );
@@ -208,8 +238,9 @@ CXParser.prototype.onclosetag = function ( tag ) {
 	} else {
 		this.print( '</' + tag + '>' );
 	}
-
-	if ( tag === 'span' && this.inReference ) {
+	// See if we have to print the left over </span>
+	// from reference handling
+	if ( tag === 'span' && this.inReference && this.sawSentenceEndCandidate ) {
 		this.print( this.endSentence() );
 	}
 };
