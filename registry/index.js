c4e40f1adcd134a390d2138959da661f3afefc30
Kartik Mistry <kartik.mistry@gmail.com>
2014-06-16 19:33:16 +0530

    Fixed typo in comment
    Change-Id: I27a1913ab6d8ddf48c8ed1858213bd03b85cacfd

--- registry/index.js
+++ registry/index.js
@@ -1,7 +1,7 @@
 var registry = require( __dirname + '/Registry.json' );
 
 /**
- * Get the available toolset for the given language pari
+ * Get the available toolset for the given language pair
  * @param {string} from source language
  * @param {string} to target language
  * @return {Object} the toolset
