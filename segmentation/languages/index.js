a3617a799ab44864693da0930a9dc571a5ccef4a
Kartik Mistry <kartik.mistry@gmail.com>
2015-03-02 12:03:02 +0530

    Add Punjabi segmentation
    Change-Id: I3f49e900ad723d330b6f1a572d0da25c0a313429

--- segmentation/languages/index.js
+++ segmentation/languages/index.js
@@ -2,9 +2,10 @@
 
 module.exports.Segmenters = {
 	en: require( __dirname + '/SegmenterEn.js' ),
-	ja: require( __dirname + '/SegmenterJa.js' ),
-	zh: require( __dirname + '/SegmenterZh.js' ),
 	hi: require( __dirname + '/SegmenterHi.js' ),
+	ja: require( __dirname + '/SegmenterJa.js' ),
+	pa: require( __dirname + '/SegmenterHi.js' ),
 	sa: require( __dirname + '/SegmenterHi.js' ),
+	zh: require( __dirname + '/SegmenterZh.js' ),
 	default: require( __dirname + '/SegmenterDefault.js' )
 };
