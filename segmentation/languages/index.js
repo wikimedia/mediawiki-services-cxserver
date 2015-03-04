2695a3166661e152ff0d65480927a88178acd878
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2015-03-04 13:51:58 +0200

    Add a segmenter for Armenian (hy)
    The sentences are separated by the Western colon (:)
    and by the Armenian sign vertsaket (։). They have similar
    appearance and both are used in the Armenian Wikipedia.
    
    The full stop is not used for sentence separation in Armenian.
    
    Bug: T91500
    Change-Id: Ib26a0b64e9bc2b29a088c9a947a585594cfe4e54

--- segmentation/languages/index.js
+++ segmentation/languages/index.js
@@ -3,6 +3,7 @@
 module.exports.Segmenters = {
 	en: require( __dirname + '/SegmenterEn.js' ),
 	hi: require( __dirname + '/SegmenterHi.js' ),
+	hy: require( __dirname + '/SegmenterHy.js' ),
 	ja: require( __dirname + '/SegmenterJa.js' ),
 	pa: require( __dirname + '/SegmenterHi.js' ),
 	sa: require( __dirname + '/SegmenterHi.js' ),
