f8dd00012ee0d9b5b559a28e2104572eda153dc1
Kartik Mistry <kartik.mistry@gmail.com>
2015-04-13 15:18:59 +0530

    config: Add requested languages and MT pairs
    Languages:
    * Bashkir (ba),
    * Chuvash (cv),
    * Perm-Komi (koi),
    * Komi (kv),
    * Lezgi (lez),
    * Sakha (sah),
    * Tuvan (tyv),
    * Udmurt (udm),
    * Asturian (ast),
    * Basque (es)
    
    MT:
    * ast<->es
    * eu<->es
    
    Bug: T95536 T95842
    Change-Id: Icbdaf480cbca91f224c9348548f18792dee83cc9

--- config.defaults.js
+++ config.defaults.js
@@ -22,21 +22,22 @@ module.exports = {
 	cert: null,
 	// Service registry
 	registry: {
-		source: [ 'af', 'an', 'ar', 'az', 'bg', 'bs', 'ca', 'cr', 'cy', 'en', 'eo', 'es', 'fr', 'gl', 'gu', 'hi', 'hr', 'id', 'ja', 'kk', 'km', 'kn', 'ky', 'kz', 'min', 'mk', 'ms', 'mt', 'nl', 'no', 'nn', 'oc', 'pa', 'pl', 'pt', 'ru', 'sh', 'simple', 'sl', 'tr', 'tt', 'uk', 'ur', 'uz', 'vi', 'xh', 'zh' ],
-		target: [ 'af', 'an', 'ar', 'az', 'bg', 'bs', 'ca', 'cr', 'cy', 'eo', 'es', 'fr', 'gl', 'gu', 'hi', 'hr', 'id', 'ja', 'kk', 'km', 'kn', 'ky', 'kz', 'min', 'mk', 'ms', 'mt', 'nl', 'no', 'nn', 'oc', 'pa', 'pl', 'pt', 'ru', 'sh', 'simple', 'sl', 'tt', 'tr', 'uk', 'ur', 'uz', 'vi', 'xh', 'zh' ],
+		source: [ 'af', 'an', 'ar', 'ast', 'az', 'ba', 'bg', 'bs', 'ca', 'cr', 'cv', 'cy', 'en', 'eo', 'es', 'eu', 'fr', 'gl', 'gu', 'hi', 'hr', 'id', 'ja', 'kk', 'km', 'kn', 'koi', 'kv', 'ky', 'kz', 'lez', 'min', 'mk', 'ms', 'mt', 'nl', 'no', 'nn', 'oc', 'pa', 'pl', 'pt', 'ru', 'sah', 'sh', 'simple', 'sl', 'tr', 'tt', 'tyv', 'udm', 'uk', 'ur', 'uz', 'vi', 'xh', 'zh' ],
+		target: [ 'af', 'an', 'ar', 'ast', 'az', 'ba', 'bg', 'bs', 'ca', 'cr', 'cv', 'cy', 'eo', 'es', 'eu', 'fr', 'gl', 'gu', 'hi', 'hr', 'id', 'ja', 'kk', 'km', 'kn', 'koi', 'kv', 'ky', 'kz', 'lez', 'min', 'mk', 'ms', 'mt', 'nl', 'no', 'nn', 'oc', 'pa', 'pl', 'pt', 'ru', 'sah', 'sh', 'simple', 'sl', 'tt', 'tr', 'tyv', 'udm', 'uk', 'ur', 'uz', 'vi', 'xh', 'zh' ],
 		mt: {
 			Apertium: {
 				af: [ 'nl' ],
 				ar: [ 'mt' ],
 				an: [ 'es' ],
+				ast: [ 'es' ],
 				bg: [ 'mk' ],
 				br: [ 'fr' ],
 				ca: [ 'es', 'en', 'eo', 'fr', 'oc', 'pt' ],
 				cy: [ 'en' ],
 				en: [ 'bs', 'ca', 'cr', 'eo', 'es', 'gl', 'hr', 'sh', 'sr' ],
 				eo: [ 'en' ],
-				es: [ 'an', 'ca', 'en', 'eo', 'fr', 'gl', 'it', 'oc', 'pt' ],
-				eu: [ 'en' ],
+				es: [ 'an', 'ast', 'ca', 'en', 'eo', 'fr', 'gl', 'it', 'oc', 'pt' ],
+				eu: [ 'en', 'es' ],
 				fr: [ 'ca', 'eo', 'es' ],
 				gl: [ 'en', 'es', 'pt' ],
 				hi: [ 'ur' ],
