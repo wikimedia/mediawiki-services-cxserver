56b5e11b059f6a67e5bdabe98d007caa37b8f62c
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2015-05-16 16:54:38 +0300

    Add segmentation for Ethiopic languages
    Bug: T98345
    Change-Id: I3a47e630dcbaff8f5b9c60b1a4d48b3db7d5606d

--- segmentation/languages/index.js
+++ segmentation/languages/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 module.exports.Segmenters = {
+	am: require( __dirname + '/SegmenterAm.js' ),
 	en: require( __dirname + '/SegmenterEn.js' ),
 	hi: require( __dirname + '/SegmenterHi.js' ),
 	hy: require( __dirname + '/SegmenterHy.js' ),
 	ja: require( __dirname + '/SegmenterJa.js' ),
 	pa: require( __dirname + '/SegmenterHi.js' ),
 	sa: require( __dirname + '/SegmenterHi.js' ),
+	ti: require( __dirname + '/SegmenterAm.js' ),
 	zh: require( __dirname + '/SegmenterZh.js' ),
 	default: require( __dirname + '/SegmenterDefault.js' )
 };
