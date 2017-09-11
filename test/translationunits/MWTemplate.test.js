868da7f4081fe280711ed7693aa9e8a6c961b914
Niklas Laxström <niklas.laxstrom@gmail.com>
2017-09-11 09:08:15 +0200

    Adapt template names if present in Wikidata
    * Bootstrap a framework for further expansion:
    ** Multi-part templates
    ** Template parameter adaptation
    ** Offline tests
    
    Change-Id: Ic90d984c92728bbbb1d245e863e8c94f047dfd56

---
+++ test/translationunits/MWTemplate.test.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const Adapter = require( '../../lib/Adapter' );
+const MWApiRequestManager = require( '../../lib/mw/ApiRequestManager' );
+const TestClient = require( '../../lib/mt' ).TestClient;
+const assert = require( '../utils/assert' );
+const async = require( 'async' );
+const cxutil = require( '../../lib/util' );
+const server = require( '../utils/server' );
+const tests = require( './MWTemplate.test.json' );
+
+describe( 'Template adaptation', () => {
+	const config = server.config;
+	config.conf.mtClient = new TestClient( config );
+
+	// Mock responses to speed up tests
+	// TODO: Move this to data driven approach
+	const realGetNamespaceAlias = MWApiRequestManager.prototype.getNamespaceAlias;
+	MWApiRequestManager.prototype.getNamespaceAlias = ( name, language ) => {
+		const promise = new cxutil.Deferred();
+		const data = {
+			sl: {
+				Template: 'Predloga'
+			}
+		};
+
+		if ( !data[ language ] || !data[ language ][ name ] ) {
+			promise.reject( new Error( `Unexpected query: namespaceAlias:${language}:${name}` ) );
+		}
+
+		promise.resolve( data[ language ][ name ] );
+		return promise;
+	};
+
+	MWApiRequestManager.titlePairCache.fi = {};
+	MWApiRequestManager.titlePairCache.fi.sl = {
+		get: ( title ) => {
+			const promise = new cxutil.Deferred();
+			const data = {
+				'./Malline:Babel': {
+					sourceTitle: 'Malline:Babel',
+					targetTitle: 'Predloga:Babilon',
+					missing: false
+				}
+			};
+
+			if ( !data[ title ] ) {
+				promise.reject( new Error( `Unexpected query: titlePair:fi:sl:${title}` ) );
+			}
+
+			promise.resolve( data[ title ] );
+			return promise;
+		}
+	};
+
+	async.forEach( tests, ( test ) => {
+		it( test.desc, () => {
+			const adapter = new Adapter( test.from, test.to, server.config );
+			const translationunit = adapter.getAdapter( test.source );
+			assert.ok( adapter, 'There is an adapter for templates' );
+
+			// Tests store format data-mw as actual JSON, we need to stringify it
+			// to make it appear as returned by parsoid
+			if ( test.source.attributes[ 'data-mw' ] ) {
+				test.source.attributes[ 'data-mw' ] = JSON.stringify( test.source.attributes[ 'data-mw' ] );
+			}
+
+			return translationunit.adapt( test.source ).then( ( adaptedNode ) => {
+				const actualDataCX = JSON.parse( adaptedNode.attributes[ 'data-cx' ] );
+				const expectedDataCX = test.result.attributes[ 'data-cx' ];
+				assert.deepEqual( expectedDataCX, actualDataCX, 'data-cx matches' );
+
+				const actualDataMW = JSON.parse( adaptedNode.attributes[ 'data-mw' ] );
+				const expectedDataMW = test.result.attributes[ 'data-mw' ];
+				assert.deepEqual( expectedDataMW, actualDataMW, 'data-mw matches' );
+			} );
+		} );
+	} );
+
+	// Cleanup
+	MWApiRequestManager.titlePairCache = new Map();
+	MWApiRequestManager.prototype.getNamespaceAlias = realGetNamespaceAlias;
+} );
