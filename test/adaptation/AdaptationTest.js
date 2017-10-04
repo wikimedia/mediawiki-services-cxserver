9f97c854ce5d9b4a7bb4a1f7df6a7313fd6e2cb4
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2017-10-04 12:43:20 +0530

    Adaptation tests: Do assertions on structured data
    Avoid DOM comparison about the text which is less relevant to adaptation.
    Follow up for  I12a723732b87
    
    Do comparison on attributes incliding data-cx, data-mw.
    Also match id attributes in source and result.
    
    While at that, MWCategory required minor updates to have proper
    data-cx.
    
    This should fix the failing tests due to strict check on MT output.
    
    Change-Id: Ife940ebce6f26cfe0bde0181a2982670add61a96

--- test/adaptation/AdaptationTest.js
+++ test/adaptation/AdaptationTest.js
@@ -3,38 +3,31 @@
 const assert = require( 'assert' ),
 	server = require( '../utils/server.js' ),
 	async = require( 'async' ),
-	Apertium = require( '../../lib/mt' ).Apertium,
+	TestClient = require( '../../lib/mt' ).TestClient,
 	Adapter = require( '../../lib/Adapter' ),
 	tests = require( './AdaptationTests.json' ),
 	jsdom = require( 'jsdom' );
 
 describe( 'Adaptation tests', () => {
 	async.forEach( tests, ( test ) => {
-		let adapter, cxserver;
-
-		cxserver = server.config.conf.services[ server.config.conf.services.length - 1 ];
-		cxserver.conf.mtClient = new Apertium( cxserver );
-		adapter = new Adapter( test.from, test.to, cxserver );
+		const cxserver = server.config.conf.services[ server.config.conf.services.length - 1 ];
+		cxserver.conf.mtClient = new TestClient( cxserver );
+		const adapter = new Adapter( test.from, test.to, cxserver );
 		it( test.desc, () => {
-			let expectedDom = new jsdom.JSDOM( test.result );
-			// Put data-cx in
-			if ( test[ 'data-cx' ] ) {
-				for ( let id in test[ 'data-cx' ] ) {
-					expectedDom.window.document.getElementById( id )
-						.setAttribute( 'data-cx', JSON.stringify( test[ 'data-cx' ][ id ] ) );
-				}
-			}
-
 			return adapter.adapt( test.source ).then( ( result ) => {
-				let actualDom = new jsdom.JSDOM( result.getHtml() );
-				if ( actualDom.window.document.body.isEqualNode( expectedDom.window.document.body ) ) {
-					assert.ok( true, test.desc );
-				} else {
-					assert.equal(
-						actualDom.window.document.body.outerHTML,
-						expectedDom.window.document.body.outerHTML,
-						test.desc
-					);
+				const actualDom = new jsdom.JSDOM( result.getHtml() );
+				for ( let id in test.resultAttributes ) {
+					assert.ok( actualDom.window.document.getElementById( id ), `Element with id ${id} exists in the result` );
+					for ( let attribute in test.resultAttributes[ id ] ) {
+						const actualAttributeValue = actualDom.window.document.getElementById( id )
+							.getAttribute( attribute );
+						const expectedAttributeValue = test.resultAttributes[ id ][ attribute ];
+						if ( typeof expectedAttributeValue === 'object' ) {
+							assert.deepEqual( JSON.parse( actualAttributeValue ), expectedAttributeValue );
+						} else {
+							assert.deepEqual( actualAttributeValue, expectedAttributeValue );
+						}
+					}
 				}
 			} );
 		} );
