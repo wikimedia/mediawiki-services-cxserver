f1db11a16a36094d282beff59bb1f91eafe5316a
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2017-10-10 15:30:29 +0530

    Remove parsoid generated section tags if any from the parsed page content
    Makes cxserver content parser ready to support T114072 - Section
    wrapping for MediaWiki sections. CX will do its own section wrapping
    based on translatable section concept.
    
    Added tests and did some code cleanup for LinearDoc.test.js
    
    Bug: T177752
    Change-Id: I743ca44b1f431e9a42a7f907922b8fe6048b6081

--- test/lineardoc/LinearDoc.test.js
+++ test/lineardoc/LinearDoc.test.js
@@ -1,54 +1,57 @@
 'use strict';
 
-var assert = require( '../utils/assert.js' ),
+const assert = require( '../utils/assert' ),
 	LinearDoc = require( '../../lib/lineardoc' ),
 	fs = require( 'fs' ),
 	transTests = require( __dirname + '/translate.test.json' );
 
-describe( 'LinearDoc', function () {
-	it( 'should be possible to linearise all kind of HTML inputs', function () {
-		var parser, testXhtmlFile, resultXmlFile, resultXhtmlFile, testXhtml, resultXml,
-			resultXhtml, i,
-			numTests = 5;
-		for ( i = 1; i <= numTests; i++ ) {
-			testXhtmlFile = __dirname + '/data/test' + i + '.xhtml';
-			resultXmlFile = __dirname + '/data/test' + i + '-result.xml';
-			resultXhtmlFile = __dirname + '/data/test' + i + '-result.xhtml';
+function normalize( html ) {
+	const normalizer = new LinearDoc.Normalizer();
+	normalizer.init();
+	normalizer.write( html.replace( /(\r\n|\n|\t|\r)/gm, '' ) );
+	return normalizer.getHtml();
+}
 
-			testXhtml = fs.readFileSync( testXhtmlFile, 'utf8' ).replace( /^\s+|\s+$/, '' );
-			resultXml = fs.readFileSync( resultXmlFile, 'utf8' ).replace( /^\s+|\s+$/, '' );
-			resultXhtml = fs.readFileSync( resultXhtmlFile, 'utf8' ).replace( /^\s+|\s+$/, '' );
-			parser = new LinearDoc.Parser( new LinearDoc.MwContextualizer() );
+describe( 'LinearDoc', () => {
+	it( 'should be possible to linearise all kind of HTML inputs', () => {
+		const numTests = 6;
+		for ( let i = 1; i <= numTests; i++ ) {
+			const testXhtmlFile = __dirname + '/data/test' + i + '.xhtml';
+			const resultXmlFile = __dirname + '/data/test' + i + '-result.xml';
+			const resultXhtmlFile = __dirname + '/data/test' + i + '-result.xhtml';
+
+			const testXhtml = fs.readFileSync( testXhtmlFile, 'utf8' ).replace( /^\s+|\s+$/, '' );
+			const expectedXml = fs.readFileSync( resultXmlFile, 'utf8' ).replace( /^\s+|\s+$/, '' );
+			const expectedXhtml = fs.readFileSync( resultXhtmlFile, 'utf8' ).replace( /^\s+|\s+$/, '' );
+			const parser = new LinearDoc.Parser( new LinearDoc.MwContextualizer() );
 			parser.init();
 			parser.write( testXhtml );
 			assert.deepEqual(
-				parser.builder.doc.dumpXml(),
-				resultXml,
+				normalize( parser.builder.doc.dumpXml() ),
+				normalize( expectedXml ),
 				'Linearised structure'
 			);
 			assert.deepEqual(
-				parser.builder.doc.getHtml(),
-				resultXhtml,
+				normalize( parser.builder.doc.getHtml() ),
+				normalize( expectedXhtml ),
 				'Reconstructed XHTML'
 			);
 		}
 	} );
 
-	it( 'should be possible to reconstruct the HTML from LinearDoc', function () {
-		var parser, textBlock1, textBlock2, i, len, test;
-
-		for ( i = 0, len = transTests.length; i < len; i++ ) {
-			test = transTests[ i ];
-			parser = new LinearDoc.Parser( new LinearDoc.MwContextualizer() );
+	it( 'should be possible to reconstruct the HTML from LinearDoc', () => {
+		for ( let i = 0, len = transTests.length; i < len; i++ ) {
+			const test = transTests[ i ];
+			const parser = new LinearDoc.Parser( new LinearDoc.MwContextualizer() );
 			parser.init();
 			parser.write( '<div>' + test.source + '</div>' );
-			textBlock1 = parser.builder.doc.items[ 1 ].item;
+			const textBlock1 = parser.builder.doc.items[ 1 ].item;
 			assert.deepEqual(
 				textBlock1.getHtml(),
 				test.source,
 				'Reconstructed source HTML'
 			);
-			textBlock2 = textBlock1.translateTags(
+			const textBlock2 = textBlock1.translateTags(
 				test.targetText,
 				test.rangeMappings
 			);
