wikimedia/mediawiki-services-cxserver

commit 58b026a9ebff3e20e4188cf9621769152194cf1a

Refresh MT client modules as classes - ES6 upgrade
Change-Id: I0a9aaf9b71ee53d093956995cbe3073001f62081

lib/mt/Youdao.js
@@ -1,64 +1,32 @@
 'use strict';
 
-var
-	util = require( 'util' ),
-	preq = require( 'preq' ),
+const preq = require( 'preq' ),
 	LinearDoc = require( './../lineardoc' ),
-	BBPromise = require( 'bluebird' ),
 	MTClient = require( './MTClient.js' ),
-	youdaoLanguageNameMap;
+	youdaoLanguageNameMap = {
+		'en>zh': 'EN2ZH_CN', // English to Chinese Simplified
+		'simple>zh': 'EN2ZH_CN', // English to Chinese Simplified
+		'en>zh-cn': 'EN2ZH_CN', // English to Chinese Simplified
+		'simple>zh-cn': 'EN2ZH_CN', // English to Chinese Simplified
+		'ja>zh-cn': 'JA2ZH_CN', // Japanese to Chinese Simplified,
+		'ja>zh': 'JA2ZH_CN', // Japanese to Chinese Simplified,
+		'ko>zh-cn': 'KR2ZH_CN', // Korean to Chinese Simplified
+		'fr>zh-cn': 'FR2ZH_CN', // Korean to Chinese Simplified
+		'ru>zh-cn': 'RU2ZH_CN', // Russian to Chinese Simplified
+		'es>zh-cn': 'SP2ZH_CN', // Spanish to Chinese Simplified
+		'zh>en': 'ZH_CN2EN', // Chinese Simplified/Traditional to English
+		'zh>simple': 'ZH_CN2EN', // Chinese Simplified/Traditional to Simple English
+		'zh>ja': 'ZH_CN2JA', // Chinese Simplified/Traditional to Japanese
+		'zh>ko': 'ZH_CN2KR', // Chinese Simplified/Traditional to Korean
+		'zh>fr': 'ZH_CN2FR', // Chinese Simplified/Traditional to French
+		'zh>ru': 'ZH_CN2RU', // Chinese Simplified/Traditional to Russian
+		'zh>es': 'ZH_CN2SP' // Chinese Simplified/Traditional to Spanish
+	};
 
-// Youdao language codes differ from the language codes that we use.
-youdaoLanguageNameMap = {
-	'en>zh': 'EN2ZH_CN', // English to Chinese Simplified
-	'simple>zh': 'EN2ZH_CN', // English to Chinese Simplified
-	'en>zh-cn': 'EN2ZH_CN', // English to Chinese Simplified
-	'simple>zh-cn': 'EN2ZH_CN', // English to Chinese Simplified
-	'ja>zh-cn': 'JA2ZH_CN', // Japanese to Chinese Simplified,
-	'ja>zh': 'JA2ZH_CN', // Japanese to Chinese Simplified,
-	'ko>zh-cn': 'KR2ZH_CN', // Korean to Chinese Simplified
-	'fr>zh-cn': 'FR2ZH_CN', // Korean to Chinese Simplified
-	'ru>zh-cn': 'RU2ZH_CN', // Russian to Chinese Simplified
-	'es>zh-cn': 'SP2ZH_CN', // Spanish to Chinese Simplified
-	'zh>en': 'ZH_CN2EN', // Chinese Simplified/Traditional to English
-	'zh>simple': 'ZH_CN2EN', // Chinese Simplified/Traditional to Simple English
-	'zh>ja': 'ZH_CN2JA', // Chinese Simplified/Traditional to Japanese
-	'zh>ko': 'ZH_CN2KR', // Chinese Simplified/Traditional to Korean
-	'zh>fr': 'ZH_CN2FR', // Chinese Simplified/Traditional to French
-	'zh>ru': 'ZH_CN2RU', // Chinese Simplified/Traditional to Russian
-	'zh>es': 'ZH_CN2SP' // Chinese Simplified/Traditional to Spanish
-};
-
-function Youdao( options ) {
-	this.logger = options.logger;
-	this.conf = options.conf;
-}
-
-util.inherits( Youdao, MTClient );
-
-/**
- * Translate marked-up text
- * Youdao does not support HTML translation. So we need to pass the plain text
- * version. We are not piping this to translateText because we want to preseve
- * the textblocks. But we cannot do annotation mapping because of complexity of
- * segmentation for CJK languages.
- *
- * @param {string} sourceLang Source language code
- * @param {string} targetLang Target language code
- * @param {string} sourceHtml Source html
- * @return {Promise} promise: Translated html
- */
-Youdao.prototype.translateHtml = function ( sourceLang, targetLang, sourceHtml ) {
-	var i, len, targetDoc, chain = [],
-		self = this;
-
-	this.buildSourceDoc( sourceHtml );
-	// Clone and adapt sourceDoc
-	targetDoc = new LinearDoc.Doc( this.sourceDoc.wrapperTag );
-
-	function translateItemDeferred( item ) {
+class Youdao extends MTClient {
+	translateItemDeferred( item, sourceLang, targetLang ) {
 		if ( item.type !== 'textblock' ) {
-			return BBPromise.resolve( item );
+			return Promise.resolve( item );
 		}
 
 		return self.translateText(
@@ -78,83 +46,98 @@ Youdao.prototype.translateHtml = function ( sourceLang, targetLang, sourceHtml )
 			};
 		} );
 	}
+	/**
+	 * Translate marked-up text
+	 * Youdao does not support HTML translation. So we need to pass the plain text
+	 * version. We are not piping this to translateText because we want to preseve
+	 * the textblocks. But we cannot do annotation mapping because of complexity of
+	 * segmentation for CJK languages.
+	 *
+	 * @param {string} sourceLang Source language code
+	 * @param {string} targetLang Target language code
+	 * @param {string} sourceHtml Source html
+	 * @return {Promise} promise: Translated html
+	 */
+	translateHtml( sourceLang, targetLang, sourceHtml ) {
+		let targetDoc, chain = [];
 
-	for ( i = 0, len = this.sourceDoc.items.length; i < len; i++ ) {
-		chain.push( translateItemDeferred( this.sourceDoc.items[ i ] ) );
+		this.buildSourceDoc( sourceHtml );
+		// Clone and adapt sourceDoc
+		targetDoc = new LinearDoc.Doc( this.sourceDoc.wrapperTag );
+
+		for ( let i = 0, len = this.sourceDoc.items.length; i < len; i++ ) {
+			chain.push( this.translateItemDeferred( this.sourceDoc.items[ i ], sourceLang, targetLang ) );
+		}
+
+		return Promise.all( chain ).then( ( results ) => {
+			targetDoc.items = results;
+			return targetDoc.getHtml();
+		} );
 	}
 
-	return BBPromise.all( chain ).then( function ( results ) {
-		targetDoc.items = results;
-		return targetDoc.getHtml();
-	} );
-};
+	translateText( sourceLang, targetLang, sourceText ) {
+		let postData, key = this.conf.mt.youdao.key;
+		if ( key === null ) {
+			return Promise.reject( new Error( 'Youdao service is misconfigured' ) );
+		}
 
-Youdao.prototype.translateText = function ( sourceLang, targetLang, sourceText ) {
-	var self = this,
-		key, postData;
-
-	key = this.conf.mt.youdao.key;
-	if ( key === null ) {
-		return BBPromise.reject( new Error( 'Youdao service is misconfigured' ) );
-	}
-
-	if ( sourceText.length > 10000 ) {
-		return BBPromise.reject( new Error( 'Source text too long: ' +
+		if ( sourceText.length > 10000 ) {
+			return Promise.reject( new Error( 'Source text too long: ' +
 			sourceLang + '-' + targetLang ) );
+		}
+
+		postData = {
+			uri: this.conf.mt.youdao.api,
+			proxy: this.conf.proxy,
+			body: {
+				key: key,
+				type: 'data',
+				doctype: 'json',
+				q: sourceText,
+				l: youdaoLanguageNameMap[ sourceLang + '>' + targetLang ],
+				transtype: 'translate'
+			}
+		};
+
+		return preq.post( postData ).then( ( response ) => {
+			if ( response.body.errorCode === 0 ) {
+				return response.body.translation[ 0 ];
+			} else {
+				throw new Error( 'Translation with Youdao failed. Error: ' +
+					this.getErrorName( response.body.errorCode ) +
+						' ' + sourceLang + '>' + targetLang );
+			}
+		} );
 	}
 
-	postData = {
-		uri: this.conf.mt.youdao.api,
-		proxy: this.conf.proxy,
-		body: {
-			key: key,
-			type: 'data',
-			doctype: 'json',
-			q: sourceText,
-			l: youdaoLanguageNameMap[ sourceLang + '>' + targetLang ],
-			transtype: 'translate'
+	/**
+	 * Returns error name from error code.
+	 *
+	 * @param {number} code Error code
+	 * @return {string}
+	 */
+	getErrorName( code ) {
+		const errormap = {
+			10: 'Some sentence in source text is too long',
+			11: 'No dictionay result',
+			20: 'Source text too long',
+			30: 'Server down',
+			40: 'Unsupported language code',
+			50: 'Invalid key',
+			52: 'IP of the request is invalid',
+			60: 'Reaching the spending limit for today',
+			70: 'Insufficinent balance'
+		};
+
+		if ( code in errormap ) {
+			return errormap[ code ];
 		}
-	};
 
-	return preq.post( postData ).then( function ( response ) {
-		if ( response.body.errorCode === 0 ) {
-			return response.body.translation[ 0 ];
-		} else {
-			throw new Error( 'Translation with Youdao failed. Error: ' +
-				self.getErrorName( response.body.errorCode ) +
-					' ' + sourceLang + '>' + targetLang );
-		}
-	} );
-};
-
-/**
- * Returns error name from error code.
- *
- * @param {number} code Error code
- * @return {string}
- */
-Youdao.prototype.getErrorName = function ( code ) {
-	var errormap = {
-		10: 'Some sentence in source text is too long',
-		11: 'No dictionay result',
-		20: 'Source text too long',
-		30: 'Server down',
-		40: 'Unsupported language code',
-		50: 'Invalid key',
-		52: 'IP of the request is invalid',
-		60: 'Reaching the spending limit for today',
-		70: 'Insufficinent balance'
-	};
-
-	if ( code in errormap ) {
-		return errormap[ code ];
+		return 'Unknown error';
 	}
 
-	return 'Unknown error';
-};
-
-Youdao.prototype.requiresAuthorization = function () {
-	return true;
-};
-
+	requiresAuthorization() {
+		return true;
+	}
+}
 module.exports = Youdao;

lib/mt/Apertium.js
@@ -1,47 +1,39 @@
 'use strict';
 
-var preq = require( 'preq' ),
-	util = require( 'util' ),
+const preq = require( 'preq' ),
 	MTClient = require( './MTClient.js' ),
-	apertiumLangMapping = require( './Apertium.languagenames.json' ),
-	postData;
+	apertiumLangMapping = require( './Apertium.languagenames.json' );
 
-function Apertium( options ) {
-	this.logger = options.logger;
-	this.conf = options.conf;
+class Apertium extends MTClient {
+	/**
+	 * Translate plain text with Apertium API
+	 * Apertium is not capable of HTML translation with all annotation
+	 * mapping. For translating HTML, It use CX's annotation mapping on top
+	 * of the plaintext translation. Hence it inherits translateHTML method
+	 * of MTClient.
+	 *
+	 * @param {string} sourceLang Source language code
+	 * @param {string} targetLang Target language code
+	 * @param {string} sourceText Source language text
+	 * @return {BBPromise} promise: Target language text
+	 */
+	translateText( sourceLang, targetLang, sourceText ) {
+		const postData = {
+			uri: this.conf.mt.apertium.api + '/translate',
+			body: {
+				markUnknown: 0,
+				langpair: apertiumLangMapping[ sourceLang ] + '|' + apertiumLangMapping[ targetLang ],
+				format: 'txt',
+				q: sourceText
+			}
+		};
+
+		return preq.post( postData )
+			.then( ( response ) => response.body.responseData.translatedText )
+			.catch( function () {
+				throw new Error( 'Translation with Apertium failed: ' + sourceLang + '-' + targetLang );
+			} );
+	}
 }
-
-util.inherits( Apertium, MTClient );
-
-/**
- * Translate plain text with Apertium API
- * Apertium is not capable of HTML translation with all annotation
- * mapping. For translating HTML, It use CX's annotation mapping on top
- * of the plaintext translation. Hence it inherits translateHTML method
- * of MTClient.
- *
- * @param {string} sourceLang Source language code
- * @param {string} targetLang Target language code
- * @param {string} sourceText Source language text
- * @return {BBPromise} promise: Target language text
- */
-Apertium.prototype.translateText = function ( sourceLang, targetLang, sourceText ) {
-	postData = {
-		uri: this.conf.mt.apertium.api + '/translate',
-		body: {
-			markUnknown: 0,
-			langpair: apertiumLangMapping[ sourceLang ] + '|' + apertiumLangMapping[ targetLang ],
-			format: 'txt',
-			q: sourceText
-		}
-	};
-
-	return preq.post( postData ).then( function ( response ) {
-		return response.body.responseData.translatedText;
-	} ).catch( function () {
-		throw new Error( 'Translation with Apertium failed: ' +
-			sourceLang + '-' + targetLang );
-	} );
-};
 
 module.exports = Apertium;

lib/mt/Yandex.js
@@ -1,100 +1,89 @@
 'use strict';
 
-var
-	util = require( 'util' ),
-	preq = require( 'preq' ),
-	BBPromise = require( 'bluebird' ),
+const preq = require( 'preq' ),
 	MTClient = require( './MTClient.js' ),
-	yandexLanguageNameMap;
+	yandexLanguageNameMap = {
+		'be-tarask': 'be', // T122033
+		nb: 'no' // T132217
+	};
 
-// Yandex language codes can differ from the language codes that
-// we use.
-yandexLanguageNameMap = {
-	'be-tarask': 'be', // T122033
-	nb: 'no' // T132217
-};
+class Yandex extends MTClient {
 
-function Yandex( options ) {
-	this.logger = options.logger;
-	this.conf = options.conf;
-}
+	/**
+	 * Translate html or plain text content with Yandex.
+	 * Yandex is capable of translating plain text and html with
+	 * annotations mapping (keeps markup retained in translated content).
+	 * Hence overriding translate method of MTClient.
+	 *
+	 * @param {string} sourceLang Source language code
+	 * @param {string} targetLang Target language code
+	 * @param {string} sourceText Source language text
+	 * @return {Q.Promise} Target language text
+	 */
+	translate( sourceLang, targetLang, sourceText ) {
+		var key, postData;
 
-util.inherits( Yandex, MTClient );
-
-/**
- * Translate html or plain text content with Yandex.
- * Yandex is capable of translating plain text and html with
- * annotations mapping (keeps markup retained in translated content).
- * Hence overriding translate method of MTClient.
- *
- * @param {string} sourceLang Source language code
- * @param {string} targetLang Target language code
- * @param {string} sourceText Source language text
- * @return {Q.Promise} Target language text
- */
-Yandex.prototype.translate = function ( sourceLang, targetLang, sourceText ) {
-	var key, postData, self = this;
-
-	key = this.conf.mt.yandex.key;
-	if ( key === null ) {
-		return BBPromise.reject( new Error( 'Yandex service is misconfigured' ) );
-	}
-
-	if ( sourceText.length > 10000 ) {
-		return BBPromise.reject( new Error( 'Source text too long: ' +
-			sourceLang + '-' + targetLang ) );
-	}
-
-	sourceLang = yandexLanguageNameMap[ sourceLang ] || sourceLang;
-	targetLang = yandexLanguageNameMap[ targetLang ] || targetLang;
-
-	postData = {
-		uri: this.conf.mt.yandex.api + '/api/v1.5/tr.json/translate',
-		proxy: this.conf.proxy,
-		body: {
-			key: key,
-			lang: sourceLang + '-' + targetLang,
-			format: 'html',
-			text: sourceText
+		key = this.conf.mt.yandex.key;
+		if ( key === null ) {
+			return Promise.reject( new Error( 'Yandex service is misconfigured' ) );
 		}
-	};
 
-	return preq.post( postData ).then( function ( response ) {
-		return response.body.text[ 0 ];
-	} ).catch( function ( response ) {
-		throw new Error( 'Translation with Yandex failed. Error: ' +
-			self.getErrorName( response.body.code ) + ' ' + sourceLang + '-' + targetLang );
-	} );
-};
+		if ( sourceText.length > 10000 ) {
+			return Promise.reject( new Error( 'Source text too long: ' +
+				sourceLang + '-' + targetLang ) );
+		}
 
-/**
- * Returns error name from error code.
- *
- * @param {number} code Error code
- * @return {string}
- */
-Yandex.prototype.getErrorName = function ( code ) {
-	// http://api.yandex.com/translate/doc/dg/reference/translate.xml
-	var errormap = {
-		200: 'ERR_OK',
-		401: 'ERR_KEY_INVALID',
-		402: 'ERR_KEY_BLOCKED',
-		403: 'ERR_DAILY_REQ_LIMIT_EXCEEDED',
-		404: 'ERR_DAILY_CHAR_LIMIT_EXCEEDED',
-		413: 'ERR_TEXT_TOO_LONG',
-		422: 'ERR_UNPROCESSABLE_TEXT',
-		501: 'ERR_LANG_NOT_SUPPORTED'
-	};
+		sourceLang = yandexLanguageNameMap[ sourceLang ] || sourceLang;
+		targetLang = yandexLanguageNameMap[ targetLang ] || targetLang;
 
-	if ( code in errormap ) {
-		return errormap[ code ];
+		postData = {
+			uri: this.conf.mt.yandex.api + '/api/v1.5/tr.json/translate',
+			proxy: this.conf.proxy,
+			body: {
+				key,
+				lang: sourceLang + '-' + targetLang,
+				format: 'html',
+				text: sourceText
+			}
+		};
+
+		return preq.post( postData )
+			.then( ( response ) => response.body.text[ 0 ] )
+			.catch( ( response ) => {
+				throw new Error( 'Translation with Yandex failed. Error: ' +
+					this.getErrorName( response.body.code ) + ' ' + sourceLang + '-' + targetLang );
+			} );
 	}
 
-	return 'Unknown error';
-};
+	/**
+	 * Returns error name from error code.
+	 *
+	 * @param {number} code Error code
+	 * @return {string}
+	 */
+	getErrorName( code ) {
+		// http://api.yandex.com/translate/doc/dg/reference/translate.xml
+		const errormap = {
+			200: 'ERR_OK',
+			401: 'ERR_KEY_INVALID',
+			402: 'ERR_KEY_BLOCKED',
+			403: 'ERR_DAILY_REQ_LIMIT_EXCEEDED',
+			404: 'ERR_DAILY_CHAR_LIMIT_EXCEEDED',
+			413: 'ERR_TEXT_TOO_LONG',
+			422: 'ERR_UNPROCESSABLE_TEXT',
+			501: 'ERR_LANG_NOT_SUPPORTED'
+		};
 
-Yandex.prototype.requiresAuthorization = function () {
-	return true;
-};
+		if ( code in errormap ) {
+			return errormap[ code ];
+		}
+
+		return 'Unknown error';
+	}
+
+	requiresAuthorization() {
+		return true;
+	}
+}
 
 module.exports = Yandex;

commit 0c9deb977ecd509f308c1469362b49f4bdc16c77

Youdao: Fix js error: self not defined
Regression from  58b026a9ebff3e20e4188cf9621769152194cf1a

Bug: T181209
Change-Id: Id361159297977a9ede31c52a6b592ac20366ec9f

lib/mt/Youdao.js
@@ -29,7 +29,7 @@ class Youdao extends MTClient {
 			return Promise.resolve( item );
 		}
 
-		return self.translateText(
+		return this.translateText(
 			sourceLang,
 			targetLang,
 			item.item.getPlainText()