e6e335a064bb2ad6665268146f5eeec70c1c055d
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-02-23 12:56:10 +0530

    Wrap the template fragments in a single section
    Bug: T187683
    Change-Id: If4755230fd6e6200bfe1c2deab13c53c8df4b43d

--- test/mw/SectionWrap.test.js
+++ test/mw/SectionWrap.test.js
@@ -31,9 +31,24 @@ const sourceHTML = `<body>
 	</section>
 	<section data-mw-section-id="2">
 	<p>Content<div>Div in paragraph</div></p>
-	<ol><li>Item</li><li>Item</li></ol>
-	<link href="./Category:Oxygen#%20" id="mwCKQ" rel="mw:PageProp/Category" />
+	<ol><li>Item</li><li>Item</li></ol></section>
+	<section data-mw-section-id="3">
+	<div typeof="mw:Transclusion" about="#mwt1" data-mw="{}">Block template</div>
 	</section>
+	<section data-mw-section-id="4">
+	<span typeof="mw:Transclusion" about="#mwt2" data-mw="{}">Some text content</span>
+	<table about="#mwt2"><tr><td>used value</td></tr></table>
+	</section>
+	<section data-mw-section-id="5">
+	<p>An inline <span typeof="mw:Transclusion" about="#mwt3" data-mw="{}">template</span></p>
+	</section>
+	<section data-mw-section-id="6">
+	<span typeof="mw:Transclusion" about="#mwt4" data-mw="{}">Template 4: Some text content</span>
+	<table about="#mwt4"><tr><td>Template 4: value</td></tr></table>
+	<span typeof="mw:Transclusion" about="#mwt5" data-mw="{}">Template 5: Some text content</span>
+	<table about="#mwt5"><tr><td>Template 5: value</td></tr></table>
+	</section>
+	<link href="./Category:Oxygen#%20" id="mwCKQ" rel="mw:PageProp/Category" />
 	</body>`;
 
 const expectedSectionWrappedHTML = `<body>
@@ -43,6 +58,24 @@ const expectedSectionWrappedHTML = `<body>
 	<section rel="cx:Section"><div>Content<div>innerdiv</div></div></section>
 	<section rel="cx:Section"><p>Content<div>Div in paragraph</div></p></section>
 	<section rel="cx:Section"><ol><li>Item</li><li>Item</li></ol></section>
+	<section rel="cx:Section">
+	<div typeof="mw:Transclusion" about="#mwt1" data-mw="{}">Block template</div>
+	</section>
+	<section rel="cx:Section">
+	<span typeof="mw:Transclusion" about="#mwt2" data-mw='{}'>Some text content</span>
+	<table about="#mwt2"><tr><td>used value</td></tr></table>
+	</section>
+	<section rel="cx:Section">
+	<p>An inline <span about="#mwt3" data-mw="{}" typeof="mw:Transclusion">template</span></p>
+	</section>
+	<section rel="cx:Section">
+	<span about="#mwt4" data-mw="{}" typeof="mw:Transclusion">Template 4: Some text content</span>
+	<table about="#mwt4"><tr><td>Template 4: value</td></tr></table>
+	</section>
+	<section rel="cx:Section">
+	<span about="#mwt5" data-mw="{}" typeof="mw:Transclusion">Template 5: Some text content</span>
+	<table about="#mwt5"><tr><td>Template 5: value</td></tr></table>
+	</section>
 	<link href="./Category:Oxygen#%20" id="mwCKQ" rel="mw:PageProp/Category" />
 	</body>`;
 
