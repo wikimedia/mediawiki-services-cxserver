eee8974227e9180a9893eef8903864b1bf7651d5
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-10-05 16:31:58 +0530

    Do not adapt the translation units under a transclusion context
    Bug: T203077
    Bug: T199529
    Change-Id: I197d93c2b76fc80460ed029030941621ab9e654f

---
+++ test/adaptation/SectionTest.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const fs = require( 'fs' );
+const Adapter = require( '../../lib/Adapter' );
+const MWApiRequestManager = require( '../../lib/mw/ApiRequestManager' );
+const TestClient = require( '../../lib/mt' ).TestClient;
+const assert = require( 'assert' );
+const jsdom = require( 'jsdom' );
+const server = require( '../utils/server' );
+
+const test = {
+	desc: 'section has lot of templates, but all are fragments of main template',
+	from: 'en',
+	to: 'es',
+	source: 'section-1.html',
+	adaptationCount: 1
+};
+
+describe( 'Adaptation tests', () => {
+	const api = new MWApiRequestManager( server.config );
+
+	it( 'should adapt section when: ' + test.desc, function ( done ) {
+		const cxserver = server.config.conf.services[ server.config.conf.services.length - 1 ];
+		cxserver.conf.mtClient = new TestClient( cxserver );
+		const adapter = new Adapter( test.from, test.to, api, cxserver );
+		let testData = fs.readFileSync( __dirname + '/data/' + test.source, 'utf8' );
+		adapter.adapt( testData ).then( ( result ) => {
+			const resultDom = new jsdom.JSDOM( result.getHtml() );
+			let elementWithDataCX = resultDom.window.document.querySelectorAll( '[data-cx]' );
+			assert.deepEqual( elementWithDataCX.length, test.adaptationCount );
+			done();
+		} );
+	} );
+} );
