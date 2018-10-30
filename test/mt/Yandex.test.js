d2bf0d7ee37843a0c6e92de5199388cf33a580ba
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-10-30 10:59:38 +0530

    Add test for MT result sanitizing
    Bug: T144467
    Change-Id: I8aaa86a419fb4f942d1b30bf3e768d9518b5f514

--- test/mt/Yandex.test.js
+++ test/mt/Yandex.test.js
@@ -4,6 +4,56 @@ const assert = require( '../utils/assert.js' );
 const server = require( '../utils/server.js' );
 const Yandex = require( '../../lib/mt' ).Yandex;
 
+const testData = {
+	input: `<section id="cxTargetSection0">
+	<p>
+	<span data-segmentid="1" class="cx-segment">This is a sentence, <i id="mwCg">
+	<a href="./Auri_(album)" rel="mw:WikiLink" data-linkid="41" class="mw-redirect cx-link" id="mwCw" title="Auri (album)">a link in italics</a></i></span>
+	</p>
+	</section>`,
+	mtIput: `<section id="cxTargetSection0">
+	<p>
+	<div id="1"><span id="2">This is a sentence, <i id="mwCg">
+	<a id="3">a link in italics</a></i></span></div>
+	</p>
+	</section>`,
+	mtResult: `<section id="cxTargetSection0">
+	<p>
+	<div id="1"><span id="2">Esta es una oración,, <i id="mwCg">
+	<a id="3" href="badLocation" onclick="bad();">un enlace en cursiva</a></i></span></div>
+	<script>Evil script</script>
+	<iframe//src=jAva&Tab;script:alert(3)>
+	</p>
+	</section>`,
+	sourceLang: 'en',
+	targetLang: 'es',
+	expectedResult: `<section id="cxTargetSection0">
+	<p>
+	<span data-segmentid="1" class="cx-segment">Esta es una oración,, <i id="mwCg">
+	<a title="Auri (album)" rel="mw:WikiLink" id="mwCw" href="./Auri_(album)" data-linkid="41" class="mw-redirect cx-link">un enlace en cursiva</a></i></span>
+	</p>
+	</section>`
+};
+
+describe( 'Yandex machine translation with corrupted result', function () {
+	it( 'Should sanitize the MT output', () => {
+		const cxConfig = server.config.service;
+		// Fake the actual Yandex call
+		const oldTranslateHTML = Yandex.prototype.translateHtml;
+		const normalize = ( html ) => html.replace( /[\t\r\n]+/g, '' );
+		Yandex.prototype.translateHtml = ( sourceLang, targetLang, mtInput ) => {
+			assert.deepEqual( normalize( mtInput ), normalize( testData.mtIput ) );
+			return Promise.resolve( testData.mtResult );
+		};
+		const yandex = new Yandex( cxConfig );
+		return yandex.translate( testData.sourceLang, testData.targetLang, testData.input )
+			.then( ( result ) => {
+				assert.deepEqual( normalize( result ), normalize( testData.expectedResult ) );
+				Yandex.prototype.translateHtml = oldTranslateHTML;
+			} );
+	} );
+} );
+
 describe( 'Yandex machine translation', function () {
 	it( 'Should fail because of wrong key ', () => {
 		const cxConfig = server.config.service;
