92c2109db0146bf21345d2425ec14376e1dcf9aa
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-11-08 16:48:24 +0530

    Reference adaptation: If content is template, use its result
    If the reference content is a template, use its adaptation
    result for the adapted property value.
    
    Test included
    
    Bug: T203160
    Change-Id: Id64dea2b56c81f6b038ec5484c6e0e30674d5d44

--- test/translationunits/MWReference.test.js
+++ test/translationunits/MWReference.test.js
@@ -7,6 +7,8 @@ const TestUtils = require( '../testutils' );
 const assert = require( '../utils/assert' );
 const async = require( 'async' );
 const server = require( '../utils/server.js' );
+const jsdom = require( 'jsdom' );
+const fs = require( 'fs' );
 
 // const mocks = require( './MWReference.mocks.json' );
 const tests = require( './MWReference.test.json' );
@@ -22,18 +24,46 @@ describe( 'Reference adaptation', () => {
 	async.each( tests, ( test, done ) => {
 		it( test.desc, () => {
 			const adapter = new Adapter( test.from, test.to, api, server.config );
-			const translationunit = adapter.getAdapter( test.source );
-
+			if ( typeof test.source === 'string' ) {
+				const content = fs.readFileSync( __dirname + '/data/' + test.source, 'utf8' );
+				const sourceDom = new jsdom.JSDOM( content );
+				const sourceDomAttributes = sourceDom.window.document.querySelector( '[typeof="mw:Extension/ref"]' ).attributes;
+				test.source = {
+					name: 'span',
+					attributes: {
+						id: sourceDomAttributes.getNamedItem( 'id' ).value,
+						'data-mw': sourceDomAttributes.getNamedItem( 'data-mw' ).value,
+						rel: 'dc:references',
+						'typeof': 'mw:Extension/ref',
+						'class': 'mw-ref'
+					}
+				};
+			}
+			if ( typeof test.result === 'string' ) {
+				const resultContent = fs.readFileSync( __dirname + '/data/' + test.result, 'utf8' );
+				const resultDom = new jsdom.JSDOM( resultContent );
+				const resultsDomAttributes = resultDom.window.document.querySelector( '[typeof="mw:Extension/ref"]' ).attributes;
+				test.result = {
+					name: 'span',
+					attributes: {
+						'data-cx': JSON.parse( resultsDomAttributes.getNamedItem( 'data-cx' ).value )
+					}
+				};
+			}
 			assert.ok( adapter, 'There is an adapter for references' );
+			const translationunit = adapter.getAdapter( test.source );
+			assert.ok( translationunit, 'There is an translationunit for content' );
 
 			return translationunit.adapt( test.source ).then( ( adaptedNode ) => {
-				const expectedDataCX = JSON.parse( adaptedNode.attributes[ 'data-cx' ] );
-				const actualDataCX = test.result.attributes[ 'data-cx' ];
+				const actualDataCX = JSON.parse( adaptedNode.attributes[ 'data-cx' ] );
+				const expectedDataCX = test.result.attributes[ 'data-cx' ];
 				assert.deepEqual( actualDataCX, expectedDataCX, 'data-cx matches' );
 
-				const expectedDataMW = JSON.parse( adaptedNode.attributes[ 'data-mw' ] );
-				const actualDataMW = test.result.attributes[ 'data-mw' ];
-				assert.deepEqual( actualDataMW, expectedDataMW, 'data-mw matches' );
+				if ( test.result.attributes[ 'data-mw' ] ) {
+					const expectedDataMW = test.result.attributes[ 'data-mw' ];
+					const actualDataMW = JSON.parse( adaptedNode.attributes[ 'data-mw' ] );
+					assert.deepEqual( actualDataMW, expectedDataMW, 'data-mw matches' );
+				}
 
 				done( null );
 			} );
