803baa469c00ca590abc7da5149cd8699df87c45
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-12-20 16:17:03 +0530

    Move the ratelimiter rate and key options to service configuration
    Bug: T210581
    Change-Id: Icbafe129282b8c1a3394a99e5041eb762bd43f75

--- app.js
+++ app.js
@@ -122,15 +122,14 @@ function initApp( options ) {
 		sUtil.initAndLogRequest( req, app );
 
 		if ( app.ratelimiter ) {
-			// eslint-disable-next-line no-underscore-dangle
-			let clientIp = req.headers[ app.ratelimiter._options.key ] ||
+			let clientIp = req.headers[ app.conf.ratelimiter_key ] ||
 				req.headers[ 'x-forwarded-for' ] ||
 				req.connection.remoteAddress;
 
-			// eslint-disable-next-line no-underscore-dangle
-			if ( app.ratelimiter.isAboveLimit( clientIp, app.ratelimiter._options.rate ) ) {
+			if ( app.ratelimiter.isAboveLimit( clientIp, app.conf.ratelimiter_rate ) ) {
 				// Too many requests, more than the configured maximum number requests per second.
 				app.metrics.increment( 'api.ratelimiter.hit' );
+				app.logger.log( 'warn', `Client ${clientIp} exceeded the configured api rate` );
 				return res.status( 429 );
 			}
 		}
