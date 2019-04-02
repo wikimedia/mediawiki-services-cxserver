1cd5d9ce22e1b2a7ab2e9d75633b4134db5ad7e7
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2019-04-02 13:32:11 -0700

    Do not translate content in the reference
    Do not translate the book titles, news paper names etc!
    
    Update the tests accordingly. Remove the it() method
    inside it() in AdaptationTest.js.
    
    Bug: T219412
    Change-Id: Ib1e2cb973a69be3edfd0fd5dff8350cd08c3690a

--- test/adaptation/AdaptationTest.js
+++ test/adaptation/AdaptationTest.js
@@ -25,27 +25,27 @@ describe( 'Adaptation tests', () => {
 			const cxserver = server.config.conf.services[ server.config.conf.services.length - 1 ];
 			cxserver.conf.mtClient = new TestClient( cxserver );
 			const adapter = new Adapter( test.from, test.to, api, cxserver );
-			it( test.desc, () => {
-				return adapter.adapt( test.source ).then( ( result ) => {
-					const actualDom = new jsdom.JSDOM( result.getHtml() );
-					for ( let id in test.resultAttributes ) {
-						assert.ok( actualDom.window.document.getElementById( id ), `Element with id ${id} exists in the result` );
-						for ( let attribute in test.resultAttributes[ id ] ) {
-							const actualAttributeValue = actualDom.window.document
-								.getElementById( id ).getAttribute( attribute );
-							const expectedAttributeValue = test.resultAttributes[ id ][ attribute ];
-							if ( typeof expectedAttributeValue === 'object' ) {
-								assert.deepEqual(
-									JSON.parse( actualAttributeValue ),
-									expectedAttributeValue
-								);
-							} else {
-								assert.deepEqual( actualAttributeValue, expectedAttributeValue );
-							}
+
+			return adapter.adapt( test.source ).then( ( result ) => {
+				const actualDom = new jsdom.JSDOM( result.getHtml() );
+
+				for ( let id in test.resultAttributes ) {
+					assert.ok( actualDom.window.document.getElementById( id ), `Element with id ${id} exists in the result` );
+					for ( let attribute in test.resultAttributes[ id ] ) {
+						const actualAttributeValue = actualDom.window.document
+							.getElementById( id ).getAttribute( attribute );
+						const expectedAttributeValue = test.resultAttributes[ id ][ attribute ];
+						if ( typeof expectedAttributeValue === 'object' ) {
+							assert.deepEqual(
+								JSON.parse( actualAttributeValue ),
+								expectedAttributeValue
+							);
+						} else {
+							assert.deepEqual( actualAttributeValue, expectedAttributeValue );
 						}
 					}
-					done( null );
-				} );
+				}
+				done( null );
 			} );
 		}, mocker.dump.bind( mocker, 'test/adaptation/AdaptationTests.mocks.json' ) );
 	} );
