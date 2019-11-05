602126136929ed0d09d52516c356577ca164df14
petarpetkovic <ppetkovic@wikimedia.org>
2019-11-05 09:02:15 +0100

    Add mw-section-number data attribute to distinct sections in an article
    In order to make a distinction where one section starts and
    which paragraphs it contains, introduce new data attribute
    called mw-section-number for every sections in segmented content.
    
    Note that mw-section is different from 'sections' - the term
    we used to annotate translation units.
    
    We need to know the mw-section-number so that we can select it for
    "section translation"
    
    Added test.
    
    Bug: T234323
    Change-Id: Id0988f99690216837e91ff8ef79273d49173be1a

---
+++ test/mw/MWPageLoaderTest.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const fs = require( 'fs' ),
+	assert = require( '../utils/assert.js' ),
+	server = require( '../utils/server.js' ),
+	BBPromise = require( 'bluebird' ),
+	async = require( 'async' ),
+	LinearDoc = require( '../../lib/lineardoc' ),
+	MWPageLoader = require( '../../lib/mw/MWPageLoader' );
+
+function normalize( html ) {
+	var normalizer = new LinearDoc.Normalizer();
+	normalizer.init();
+	normalizer.write( html.replace( /[\t\r\n]+/gm, '' ) );
+	return normalizer.getHtml();
+}
+
+const tests = [
+	{
+		desc: 'Add data-section-number attribute to every CX section',
+		source: 'test-data-section-number.html',
+		sourceLanguage: 'en',
+		targetLanguage: 'es',
+		result: 'result-data-section-number.html'
+	}
+];
+describe( 'MWPageLoader tests', () => {
+	async.each( tests, ( test ) => {
+		it( 'Test: ' + test.desc, function () {
+			// Fake the actual MWPageLoader fetch call
+			MWPageLoader.prototype.fetch = () => {
+				const sourceContent = fs.readFileSync( __dirname + '/data/' + test.source, 'utf8' );
+				return BBPromise.delay( 0 ).then( () => ( { body: sourceContent } )
+				);
+			};
+			const pageloader = new MWPageLoader( {
+				context: server.config,
+				sourceLanguage: test.sourceLanguage,
+				targetLanguage: test.targetLanguage
+			} );
+			const expectedResultData = normalize(
+				fs.readFileSync( __dirname + '/data/' + test.result, 'utf8' )
+			);
+			return pageloader.getPage( 'mockPage', null, true ).then( ( processedPageContent ) => {
+				assert.deepEqual( normalize( processedPageContent.content ), expectedResultData, test.desc );
+			} );
+		} );
+	} );
+} );
