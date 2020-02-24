8152995f71229385efdf206a1b901943af38a7a1
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-02-24 18:14:20 +0530

    API for finding missing sections in an article pair
    Introduce /suggest/sections/title/from/to API
    
    It returns the sections present in source language article and not
    present in target language.
    
    The section mapping is based on the database with section title mapping
    based on ContentTranslation parallel corpus.
    
    Tests included
    
    Bug: T243430
    Change-Id: I7b340344f6246a66596a6c05b4b84a754d6fdf99

---
+++ test/suggestions/SectionSuggestion.test.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const assert = require( '../utils/assert.js' ),
+	async = require( 'async' ),
+	server = require( '../utils/server.js' ),
+	SectionSuggester = require( '../../lib/suggestion/SectionSuggester' ),
+	MWApiRequestManager = require( '../../lib/mw/ApiRequestManager' );
+
+const tests = [
+	{
+		sourceLanguage: 'en',
+		targetLanguage: 'ml',
+		sourceTitle: 'Sitar',
+		targetTitle: 'സിത്താർ',
+		sourceSections: [
+			'Etymology',
+			'History',
+			'Playing',
+			'See also',
+			'External links',
+			'References'
+		],
+		targetSections: [
+			'അവലംബം'
+		],
+		expectedResult: {
+			sourceLanguage: 'en',
+			sourceTitle: 'Sitar',
+			targetLanguage: 'ml',
+			targetTitle: 'സിത്താർ',
+			present: {
+				References: 'അവലംബം'
+			},
+			missing: {
+				Etymology: 'പേരിന്റെ ഉത്ഭവം',
+				History: 'ചരിത്രം',
+				Playing: 'വായിക്കുന്ന രീതി',
+				'See also': 'ഇതും കാണുക',
+				'External links': 'പുറത്തേക്കുള്ള കണ്ണികൾ'
+			}
+		}
+	}
+];
+
+describe( 'SectionSuggester tests', () => {
+	async.forEach( tests, ( test ) => {
+		it( 'should extract correct number of categories when ' + test.desc, () => {
+			const cxConfig = server.config.service;
+			const api = new MWApiRequestManager( cxConfig );
+			SectionSuggester.prototype.getSections = ( language ) => {
+				if ( language === test.sourceLanguage ) {
+					return Promise.resolve( test.sourceSections );
+				}
+				if ( language === test.targetLanguage ) {
+					return Promise.resolve( test.targetSections );
+				}
+			};
+
+			const sectionSuggestor = new SectionSuggester(
+				api,
+				cxConfig.conf.sectionmapping.database
+			);
+
+			sectionSuggestor.getMissingSections( test.sourceLanguage, test.sourceTitle, test.targetLanguage, test.targetTitle ).then( ( sections ) => {
+				assert.deepEqual( sections, test.expectedResult );
+			} );
+		} );
+	} );
+} );
