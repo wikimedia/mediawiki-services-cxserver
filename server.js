b61e7edb1a5fdfc320d021a939c43e3017984a03
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2024-09-19 12:32:29 +0530

    Add -c option to server.js to specify config file
    Change-Id: Ibd47364da89a2a1582c0acb5a6a23d3017aeb16b

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 'use strict';
 
 const app = require( './app.js' );
+const yargs = require( 'yargs' );
 const { getConfig } = require( './lib/util.js' );
 
-app( getConfig() );
+yargs.option( 'c', {
+	alias: 'config',
+	describe: 'Path to the config file',
+	type: 'string',
+	coerce: ( arg ) => require( 'path' ).resolve( arg ),
+	default: `${ __dirname }/config.yaml`
+} );
+const configPath = yargs.argv.c;
+
+console.log( 'Using config file:', configPath );
+app( getConfig( configPath ) );
